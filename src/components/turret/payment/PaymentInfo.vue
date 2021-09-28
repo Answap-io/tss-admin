477b8f2761d72bd1fbe7bc5d5b7ffb59ed3c3d23
Hernán Peralta <alegodemencia@gmail.com>
2021-09-28 12:39:18 -0300

    Create PaymentInfo component

---
+++ src/components/turret/payment/PaymentInfo.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="transaction-fee mt-4 flex flex-col">
+    <div class="overflow-x-auto -mx-8">
+      <div class="py-2 align-middle">
+        <div class="shadow overflow-hidden border-b border-gray-200 rounded-lg">
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+              <tr>
+                <th
+                  class="
+                    px-6
+                    py-3
+                    text-left text-xs
+                    font-medium
+                    text-gray-500
+                    uppercase
+                    tracking-wider
+                  "
+                  scope="col"
+                >
+                  Field
+                </th>
+                <th
+                  class="
+                    px-6
+                    py-3
+                    text-left text-xs
+                    font-medium
+                    text-gray-500
+                    uppercase
+                    tracking-wider
+                  "
+                  scope="col"
+                >
+                  Value
+                </th>
+              </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  Public Key
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {{ json.publicKey }}
+                </td>
+              </tr>
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  Payment Hash
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {{ json.paymentHash }}
+                </td>
+              </tr>
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  Last modified time
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {{ new Date(Number(json.lastModifiedTime)) }}
+                </td>
+              </tr>
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  Balance
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {{ json.balance }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    json: {
+      type: Object,
+      required: true,
+    },
+  },
+})
+export default class PaymentInfo extends Vue {}
+</script>
